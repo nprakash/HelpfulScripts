eb42f237c9a5be9e3230914606e3f16c284d4e22
Navya Prakash <navya.prakash92@gmail.com>
2019-08-28 18:13:38 -0700

    Removing brew uninstall

--- destroy-mysql.sh
+++ destroy-mysql.sh
@@ -5,15 +5,15 @@ brew cleanup
 read -p '>>>> Would you like me to search for where mysql is installed [search/default] ' SEARCH
 if [[ "{SEARCH}" == 'search' || "{SEARCH}" == 'Search' || "{SEARCH}" == 'SEARCH' ]]
 then
-MYSQL=$(which mysql)
-echo "${MYSQL}"
-sudo ${MYSQL}/support-files/mysql.server stop
-sudo rm ${MYSQL}
-sudo rm -rf ${MYSQL}*
+    MYSQL=$(which mysql)
+    echo "${MYSQL}"
+    sudo ${MYSQL}/support-files/mysql.server stop
+    sudo rm ${MYSQL}
+    sudo rm -rf ${MYSQL}*
 else
-sudo /usr/local/mysql/support-files/mysql.server stop
-sudo rm /usr/local/mysql
-sudo rm -rf /usr/local/mysql*
+    sudo /usr/local/mysql/support-files/mysql.server stop
+    sudo rm /usr/local/mysql
+    sudo rm -rf /usr/local/mysql*
 fi
 
 sudo rm /usr/local/mysql
@@ -27,6 +27,6 @@ rm -rf ~/Library/PreferencePanes/My*
 sudo rm -rf /Library/Receipts/mysql*
 sudo rm -rf /Library/Receipts/MySQL*
 sudo rm -rf /private/var/db/receipts/*mysql*
-echo ">>> Mysql removed, removing brew"
-ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/uninstall)"
-echo ">>> Brew removed -- Restart your computer and then re-install brew and mysql (w/brew)"
+# echo ">>> Mysql removed, removing brew"
+# ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/uninstall)"
+# echo ">>> Brew removed -- Restart your computer and then re-install brew and mysql (w/brew)"
